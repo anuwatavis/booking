8e30ab0b78085a103b83d4e410c440cc8ee49a17
Goragod <admin@goragod.com>
2019-01-24 08:34:07 +0700

    format Code

--- install/settings/config.php
+++ install/settings/config.php
@@ -1,24 +1,23 @@
 <?php
-
 /* config.php */
 
 return array(
-    'version' => '2.0.2',
-    'web_title' => 'E-Booking',
-    'web_description' => 'ระบบจองห้องประชุม',
-    'timezone' => 'Asia/Bangkok',
-    'member_status' => array(
-        0 => 'สมาชิก',
-        1 => 'ผู้ดูแลระบบ',
-        2 => 'ผู้รับผิดชอบ',
-    ),
-    'color_status' => array(
-        0 => '#259B24',
-        1 => '#FF0000',
-        2 => '#0000FF',
-    ),
-    'user_forgot' => 0,
-    'user_register' => 0,
-    'welcome_email' => 0,
-    'booking_w' => 500,
+  'version' => '2.0.2',
+  'web_title' => 'E-Booking',
+  'web_description' => 'ระบบจองห้องประชุม',
+  'timezone' => 'Asia/Bangkok',
+  'member_status' => array(
+    0 => 'สมาชิก',
+    1 => 'ผู้ดูแลระบบ',
+    2 => 'ผู้รับผิดชอบ',
+  ),
+  'color_status' => array(
+    0 => '#259B24',
+    1 => '#FF0000',
+    2 => '#0000FF',
+  ),
+  'user_forgot' => 0,
+  'user_register' => 0,
+  'welcome_email' => 0,
+  'booking_w' => 500,
 );
